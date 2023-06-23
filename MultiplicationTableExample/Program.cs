35b437d70f14009d28ed995b2f4f0dc6037e1980
a454922 <a454922@PLWROW4HMCLR3>
2023-06-23 13:45:24 +0200

    Add project files.

---
+++ MultiplicationTableExample/Program.cs
@@ -0,0 +1,19 @@
+﻿void printMultTable(int number) //here's our function
+{
+    for (int i = 2; i < 10; i++)
+    {
+        Console.WriteLine($"{number}*{i} = {i * number}");
+    }
+}
+
+//here goes the main part
+int keyAscii = 50;//ASCII code between 48 and 58
+
+Console.Write("Press a key [1-9] to view the corresponding multiplication table, ESC or ENTER to exit: ");
+while (keyAscii != 27 && keyAscii != 13) //ASCII code of ESC and ENTER
+{
+    keyAscii = Console.ReadKey().KeyChar; //implicit cast from Char to Int
+    Console.Write('\n');
+    if (keyAscii > 48 && keyAscii < 58) printMultTable(keyAscii - 48); //here 48 is the offset between digits and their ASCII code
+    else Console.WriteLine("Incorrect input");
+}
---
+++ SwitchExample/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+do
+{
+    Console.Write("\nPress the number [1-7] and get the corresponding day of the week: ");
+    switch (Console.ReadKey().Key)
+    {
+        case ConsoleKey.D1:
+            Console.WriteLine(" - Monday");
+            break;
+        case ConsoleKey.D2:
+            Console.WriteLine(" - Tuesday");
+            break;
+        case ConsoleKey.D3:
+            Console.WriteLine(" - Wednesday");
+            break;
+        case ConsoleKey.D4:
+            Console.WriteLine(" - Thursday");
+            break;
+        case ConsoleKey.D5:
+            Console.WriteLine(" - Friday");
+            break;
+        case ConsoleKey.D6:
+            Console.WriteLine(" - Saturday");
+            break;
+        case ConsoleKey.D7:
+            Console.WriteLine(" - Sunday");
+            break;
+        default:
+            Console.WriteLine(" - !Nonsence!\b");
+            break;
+    }
+    Console.Write("Again? N to exit, any other key to continue: ");
+} while (Console.ReadKey().Key != ConsoleKey.N);
